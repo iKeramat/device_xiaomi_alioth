42861f1300ba68eedf4462f76b62523a58e9666b
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-11-13 10:14:14 +0330

    alioth: Bringup LMODroid
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
